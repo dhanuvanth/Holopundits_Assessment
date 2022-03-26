e78d403fbe95f6574023f7c76d5c3e2947034b9c
dhanuvanth <43810498+dhanuvanth@users.noreply.github.com>
2022-03-26 15:19:54 +0530

    Initial

---
+++ Assets/LiftController.cs
@@ -0,0 +1,64 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LiftController : MonoBehaviour
+{
+    float speed = 5f;
+    public Transform pos1;
+    public Transform pos2;
+    bool trigger = false;
+    public GameObject lift;
+
+    bool targetPos = false;
+    int count = 0;
+
+    public Text score;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            Invoke("Trigger", 2f);
+            GameObject player = collision.collider.gameObject;
+            player.transform.parent = transform;
+            Debug.Log(trigger);
+            targetPos = true;
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            
+            GameObject player = collision.collider.gameObject;
+            player.transform.parent = null;
+           
+        }
+    }
+
+    void Trigger()
+    {
+        trigger = !trigger;
+        count++;
+
+        score.text = count.ToString();
+    }
+
+    private void Update()
+    {
+        if (targetPos)
+        {
+            if (trigger)
+            {
+                transform.DOMoveY(10f,5f);
+                
+            }
+            else
+            {
+                transform.DOMoveY(0, 5f);
+            }
+        }
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    float speed = 5f;
+
+    float hm, vm;
+
+    private void Update()
+    {
+        vm = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        hm = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        transform.Translate(-vm, 0, hm);
+    }
+}
